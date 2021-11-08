92872f364e9a24de391ba1f386a9ca488cf73bb2
Yarema Medynskyi <yaremamed2@gmail.com>
2021-11-08 12:51:45 +0200

    Move back to uri((

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from resources.withdrawal import Withdrawal, Withdrawals
 ACCESS_EXPIRES = timedelta(minutes=10)
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = ACCESS_EXPIRES
